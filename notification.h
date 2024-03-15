45eec27ee6e9e93cb7d9a47723fb21cc358d70c1
zhaaimin <861271193@qq.com>
2024-03-15 16:48:18 +0800

    Update notification.h

--- notification.h
+++ notification.h
@@ -37,13 +37,13 @@ namespace notification
     class NotificationCenter
     {
     public:
-    // 获取单例实例
+   
     static NotificationCenter& getInstance() {
         static NotificationCenter instance;
         return instance;
     }
 
-    // 添加观察者
+    //register notification
     template<typename T>
     void addObserver(const std::string &name, NotificationObserver<T> callback) {
         std::cout<<"addObserver name:"<<name<<std::endl;
@@ -53,7 +53,7 @@ namespace notification
         observers<T>[name] = arr;
     };
 
-    // 发送通知
+    // post noticication
     template<typename T>
     void postNotification(const std::string &name,const NotificationInfo<T>& notification) {
         std::cout<<"postNotification name:"<<name<<std::endl;
@@ -72,11 +72,11 @@ namespace notification
     };
 
 private:
-    NotificationCenter() {} // 构造函数私有化，防止外部创建对象
-    NotificationCenter(const NotificationCenter&) = delete; // 禁止拷贝构造
-    NotificationCenter& operator=(const NotificationCenter&) = delete; // 禁止赋值操作
+    NotificationCenter() {} 
+    NotificationCenter(const NotificationCenter&) = delete; 
+    NotificationCenter& operator=(const NotificationCenter&) = delete; 
 
-    std::mutex mutex_; // 互斥锁，用于保护观察者列表
+    std::mutex mutex_; 
 };
     
 } // namespace notification
